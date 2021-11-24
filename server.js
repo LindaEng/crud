c0df4fd98a1c92e5a8f8885fcdf66863fc739442
= <90lineng@gmail.com>
2021-11-24 11:42:26 -0500

    CRUD exercises added

--- server.js
+++ server.js
@@ -13,12 +13,17 @@ seed()
 app.use(express.json())
 //ROUTES
 
+//EXERCISE: Clean up the code
+
 //C - post
-app.post('/music', async (req, res) => {
+app.post('/getmusic', async (req, res) => {
     let newSong = req.body
     await Music.create(newSong)
     res.send(`New song added~`)
 })
+
+//Express validator
+
 
 //R - get
 app.get('/music', async (req, res) => {
@@ -26,15 +31,33 @@ app.get('/music', async (req, res) => {
     res.json({allMusic})
 })
 
-app.get('/music/:id', async (req, res) => {
+app.get('/music/id', async (req, res) => {
     let id = req.params.id
     let oneSong = await Music.findByPk(id)
     res.json({oneSong})
 })
 
+//Challenge: A user only wants a specific genre of music, write a RESTful route/endpoint that will return the specified genre
+
+
+
 //U - put
+app.put('/music/:id', async (req, res) => {
+    let id = req.params.id
+    let songToUpdate = await Music.findByPk(id)
+    let updatedSong = await songToUpdate.update(req.body)
+    if(updatedSong) {
+        res.status(201).send(`Song has been updated!`)
+    } 
+})
 
 //D - delete
+app.delete('/music/:id', async (req, res) => {
+    let id = req.params.id
+    let songToDelete = await Music.findByPk(id)
+    await songToDelete.destroy()
+    res.status(201).send(`Song has been deleted!`)
+})
 
 
 app.listen(PORT, () => {
